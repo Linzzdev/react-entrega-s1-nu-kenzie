2df3524c669bc4f3d708eb7995c5bf514396d0db
Linzzdev <158218407+Linzzdev@users.noreply.github.com>
2024-06-03 16:52:51 -0300

    Att
    att

--- vite-project/src/components/Header/header.jsx
+++ vite-project/src/components/Header/header.jsx
@@ -3,7 +3,9 @@ import styles from './style.module.scss'
 export const Header = () =>{
     return(
         <header className={styles.header} >
+            <div className={styles.container}>
             <h1>Nu <span>Kenzie</span></h1>
+            </div>
         </header>
     )
 }
